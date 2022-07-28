caca1ae4633480d6c0345d4e8a008aa8dd3d901b
Nadav <nadavhadad8@gmail.com>
2022-07-29 00:16:11 +0300

    h

--- Final Project/src/graphics.erl
+++ Final Project/src/graphics.erl
@@ -26,18 +26,29 @@ start() ->
 init(_Args) ->
 	WxServer = wx:new(),
 	Frame = wxFrame:new(WxServer, ?wxID_ANY, "Don't Touch The Spikes - Nadav & Tamir", [{size,{?BG_WIDTH, ?BG_HEIGHT}}]),
-	_Panel  = wxPanel:new(Frame,[{size, {?BG_WIDTH, ?BG_HEIGHT}}]),
+	Panel  = wxPanel:new(Frame,[{size, {?BG_WIDTH, ?BG_HEIGHT}}]),
 	_Button = wxButton:new(Frame, 10, [{label, "Start"}]),
 
-	add_images(),
+	MainSizer = wxBoxSizer:new(?wxHORIZONTAL),
+	wxSizer:add(MainSizer, Panel,[{flag,?wxEXPAND}]),
+	add_images(Panel),
 
 	wxFrame:show(Frame),
+	wxPanel:connect(Panel, paint, [callback]),
 	timer:sleep(5000)
 	.
 
 handle_event(_Request, _State) ->
 	todo.
 
-add_images() ->
-
+add_images(Panel) ->
+	Rmap = wxImage:new("images/background.png"),
+	Rmapc = wxImage:scale(Rmap,?BG_WIDTH,?BG_HEIGHT),
+	BmpRMap = wxBitmap:new(Rmapc),
+	wxImage:destroy(Rmap),
+	wxImage:destroy(Rmapc),
+	DC2 = wxPaintDC:new(Panel),
+	wxDC:clear(DC2),
+	wxDC:drawBitmap(DC2,BmpRMap,{10,10}),
+	wxPaintDC:destroy(DC2)
 	.
