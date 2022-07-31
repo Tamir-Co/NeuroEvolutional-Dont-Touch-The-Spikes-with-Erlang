d9dd89b9cc9b79ff40a2ef76158f62bc1853b265
Nadav <nadavhadad8@gmail.com>
2022-07-31 19:58:53 +0300

    4 funcs work

--- Final Project/src/graphics.erl
+++ Final Project/src/graphics.erl
@@ -13,12 +13,16 @@
 -include_lib("wx/include/wx.hrl").
 -behaviour(wx_object).
 -export([start/0]).
--export([init/1, handle_event/2]).
+-export([init/1, handle_event/2, handle_sync_event/3, handle_info/2]).
 
 -define(SERVER, ?MODULE).
 
 -define(BG_WIDTH,500).
 -define(BG_HEIGHT,1000).
+-define(Timer,100).% Graphics Update Timer, default: 60
+
+%% GRAPHICS RECORDS %%
+-record(graphics_state, {frame, panel, bitmapBG, bitmapBird}).
 
 start() ->
 	wx_object:start({local,?SERVER},?MODULE,[],[]).
@@ -27,31 +31,57 @@ init(_Args) ->
 	WxServer = wx:new(),
 	Frame = wxFrame:new(WxServer, ?wxID_ANY, "Don't Touch The Spikes - Nadav & Tamir", [{size,{?BG_WIDTH, ?BG_HEIGHT}}]),
 	Panel  = wxPanel:new(Frame,[{size, {?BG_WIDTH, ?BG_HEIGHT}}]),
-	_Button = wxButton:new(Frame, 10, [{label, "Start"}]),
+	Button = wxButton:new(Frame, 10, [{label, "Start"}]),
 
-	MainSizer = wxBoxSizer:new(?wxVERTICAL),
-	SizerBird = wxStaticBoxSizer:new(?wxHORIZONTAL, Panel, [{label, "SizerBird"}]),
-	SizerBG = wxStaticBoxSizer:new(?wxVERTICAL, Panel, [{label, "SizerBG"}]),
+	MainSizer = wxBoxSizer:new(?wxHORIZONTAL),
+	UiSizer = wxBoxSizer:new(?wxVERTICAL),
+
+	wxSizer:add(MainSizer, Panel,[{flag,?wxEXPAND}]),
+	wxSizer:add(UiSizer, Button,[{flag,?wxALL bor ?wxEXPAND},{border, 2}]),
+	wxSizer:add(MainSizer, UiSizer),
 
 	ImageBG = wxImage:new("images/background.png", []),
 	BitmapBG = wxBitmap:new(wxImage:scale(ImageBG, ?BG_WIDTH, ?BG_HEIGHT, [{quality, ?wxIMAGE_QUALITY_HIGH}])),
-	StaticBitmapBG = wxStaticBitmap:new(Panel, 1, BitmapBG),
-	wxSizer:add(SizerBG, StaticBitmapBG, []),
-
+%%	StaticBitmapBG = wxStaticBitmap:new(Panel, 1, BitmapBG),
 
 	ImageBird = wxImage:new("images/bird.png", []),
 	BitmapBird = wxBitmap:new(wxImage:scale(ImageBird, 50, 50, [])),
-	StaticBitmapBird = wxStaticBitmap:new(Panel, 1, BitmapBird),
-	wxSizer:add(SizerBird, StaticBitmapBird, []),
+%%	StaticBitmapBird = wxStaticBitmap:new(Panel, 1, BitmapBird),
 
-	wxSizer:add(MainSizer, SizerBG, []),
-	wxSizer:add(MainSizer, SizerBird, []),
-%%	wxPanel:setSizer(Panel, MainSizer),
+	wxPanel:setSizer(Frame, MainSizer),
+	wxSizer:setSizeHints(MainSizer, Frame),
 
 	wxFrame:show(Frame),
+	erlang:send_after(?Timer, self(), timer),
 
-	timer:sleep(10000)
+	wxPanel:connect(Panel, paint, [callback]),
+	wxButton:connect(Button, command_button_clicked),
+
+	%timer:sleep(5000),
+
+	{Frame, #graphics_state{
+				frame = Frame,
+				panel = Panel,
+				bitmapBG = BitmapBG,
+				bitmapBird = BitmapBird}}
 .
 
-handle_event(_Request, _State) ->
-	todo.
+handle_event(Request, State) ->
+	io:format("Started! request: ~p", [Request]),
+	{noreply, State}.
+
+handle_info(timer, State = #graphics_state{frame = Frame}) ->  % refresh screen for graphics
+	wxWindow:refresh(Frame), % refresh screen
+	erlang:send_after(?Timer, self(), timer),
+	io:format("handle_info~n"),
+	{noreply, State}.
+
+handle_sync_event(Event, _, _State = #graphics_state{panel = Panel, bitmapBG = BitmapBG, bitmapBird = BitmapBird}) ->
+	DC2 = wxPaintDC:new(Panel),
+	wxDC:clear(DC2),
+	wxDC:drawBitmap(DC2, BitmapBG, {0,0}),
+	wxDC:drawBitmap(DC2, BitmapBird, {220,400}),
+%%	wxBitmap:destroy(BitmapBird),
+%%	wxBitmap:destroy(BitmapBG),
+	io:format("handle_sync_event~n Event: ~p~n~n", [Event]),
+	wxPaintDC:destroy(DC2).
