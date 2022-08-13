1ccd2d69417b2089cf4f30162fae5bce76a88814
Tamir-Co <tamirapple2000@gmail.com>
2022-08-14 00:21:36 +0300

    Update graphics.erl

--- Final Project/src/graphics.erl
+++ Final Project/src/graphics.erl
@@ -354,10 +354,19 @@ merge_birds(BestCandBirds, CandBirds) ->
 	merge_birds(BestCandBirds, CandBirds, [], ?NUM_OF_SURVIVED_BIRDS).	% we only choose ceil(0.1*N) of all birds
 
 merge_birds(_, _, BestCandBirds, 0) -> BestCandBirds;
-merge_birds([{BirdPID1, FrameCount1}|CandBirds1], [{BirdPID2, FrameCount2}|CandBirds2], BestCandBirds, BirdsToChoose) when FrameCount2 < FrameCount1 ->
-	merge_birds(CandBirds1, [{BirdPID2, FrameCount2}|CandBirds2], BestCandBirds ++ [{BirdPID1, FrameCount1}], BirdsToChoose-1);
-merge_birds([{BirdPID1, FrameCount1}|CandBirds1], [{BirdPID2, FrameCount2}|CandBirds2], BestCandBirds, BirdsToChoose) ->
-	merge_birds([{BirdPID1, FrameCount1}|CandBirds1], CandBirds2, BestCandBirds ++ [{BirdPID2, FrameCount2}], BirdsToChoose-1).
+merge_birds([Bird1|CandBirds1], [Bird2|CandBirds2], BestCandBirds, BirdsToChoose) ->
+	{_BirdPID1, {FrameCount1, _WeightsMap1}} = Bird1,
+	{_BirdPID2, {FrameCount2, _WeightsMap2}} = Bird2,
+	case FrameCount1 < FrameCount2 of
+		true  -> merge_birds([Bird1|CandBirds1], CandBirds2, BestCandBirds ++ [Bird2], BirdsToChoose-1);
+		false -> merge_birds(CandBirds1, [Bird2|CandBirds2], BestCandBirds ++ [Bird1], BirdsToChoose-1)
+	end.
+
+% merge_birds(_, _, BestCandBirds, 0) -> BestCandBirds;
+% merge_birds([{BirdPID1, FrameCount1}|CandBirds1], [{BirdPID2, FrameCount2}|CandBirds2], BestCandBirds, BirdsToChoose) when FrameCount2 < FrameCount1 ->
+	% merge_birds(CandBirds1, [{BirdPID2, FrameCount2}|CandBirds2], BestCandBirds ++ [{BirdPID1, FrameCount1}], BirdsToChoose-1);
+% merge_birds([{BirdPID1, FrameCount1}|CandBirds1], [{BirdPID2, FrameCount2}|CandBirds2], BestCandBirds, BirdsToChoose) ->
+	% merge_birds([{BirdPID1, FrameCount1}|CandBirds1], CandBirds2, BestCandBirds ++ [{BirdPID2, FrameCount2}], BirdsToChoose-1).
 
 
 %% Notify all PCs about their best birds. A bird performs better when it stays alive for more frames.
